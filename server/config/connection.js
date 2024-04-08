75e6dbb1b7358392e6f312e344d3050026e0efa6
TNielson78 <travis.nielson01@gmail.com>
2024-04-07 18:25:26 -0600

    changed connection file

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
-
+// mongoose.connect( "mongodb+srv://travisnielson01:8JVckEyv12BLOcI5@cluster0.24ilvcw.mongodb.net/googlebooks?retryWrites=true&w=majority&appName=Cluster0")
 module.exports = mongoose.connection;
